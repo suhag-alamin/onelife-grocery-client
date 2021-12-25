e9218162b63ff4f5de226f0af7a625b9bd9fbe21
Suhag Al Amin <developersuhag@gmail.com>
2021-12-25 15:22:55 +0600

    weekly deals

---
+++ components/HomeComponents/WeeklyDeals/index.js
@@ -0,0 +1,56 @@
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+import { Button, Col, Container, Row, Spinner } from "react-bootstrap";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchGroceries } from "../../../redux/slices/grocerySlice";
+import shopStyles from "../../../styles/Shop.module.scss";
+import SingleGrocery from "../../SingleGrocery";
+
+const WeeklyDeals = () => {
+  // dispatch
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchGroceries());
+  }, [dispatch]);
+
+  const { oneLifeGrocery } = useSelector((state) => state);
+  const { groceries } = oneLifeGrocery;
+  const router = useRouter();
+  console.log(oneLifeGrocery);
+  // loading spinner
+  if (oneLifeGrocery.status === "pending") {
+    return (
+      <div className="text-center py-2">
+        <Spinner animation="border" />
+      </div>
+    );
+  }
+
+  const handleClick = () => {
+    router.push("/shop");
+  };
+  return (
+    <Container className={`${shopStyles.WeeklyDeals} py-5`}>
+      <h3 className={`${shopStyles.title} text-center mb-4`}>Weekly deals</h3>
+      <Row xs={1} md={2} lg={4} className="g-4">
+        {groceries?.slice(4, 8)?.map((grocery) => (
+          <Col key={grocery._id}>
+            <SingleGrocery grocery={grocery} />
+          </Col>
+        ))}
+      </Row>
+      <div className="d-flex justify-content-center text-center mt-4">
+        <Button
+          onClick={handleClick}
+          className="d-flex align-items-center gap-2"
+        >
+          View all groceries
+          <AiOutlineArrowRight />
+        </Button>
+      </div>
+    </Container>
+  );
+};
+
+export default WeeklyDeals;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Banner from "../components/HomeComponents/Banner";
 import Features from "../components/HomeComponents/Features";
 import LatestProdcuts from "../components/HomeComponents/LatestProdcuts";
 import ProductCategory from "../components/HomeComponents/ProductCategory";
+import WeeklyDeals from "../components/HomeComponents/WeeklyDeals";
 
 export default function Home() {
   return (
@@ -14,6 +15,8 @@ export default function Home() {
       <LatestProdcuts />
       {/* product catergory  */}
       <ProductCategory />
+      {/* WeeklyDeals  */}
+      <WeeklyDeals />
     </div>
   );
 }
